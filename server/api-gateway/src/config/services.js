90a78bf72d3b26d5c0f96cbf0f31f4936d7314a1
soumen-github-account <soumendas7584@gmail.com>
2026-09-11 18:40:43 +0530

    services deploy add

--- server/api-gateway/src/config/services.js
+++ server/api-gateway/src/config/services.js
@@ -1,11 +1,11 @@
 const services = {
-    auth: "http://localhost:8000",
+    auth:   process.env.AUTH_SERVICE, //"http://localhost:8000",
     user:   process.env.USER_SERVICE, // "https://ecommerce-system-user-service.onrender.com", // http://localhost:5002
-    seller: "http://localhost:5003",
-    product: "http://localhost:5004",
+    seller: process.env.SELLER_SERVICE, //"http://localhost:5003",
+    product: process.env.PRODUCT_SERVICE, //"http://localhost:5004",
     inventory: "http://localhost:5005",
-    order: "http://localhost:5006",
-    payment: "http://localhost:5007",
+    order: process.env.ORDER_SERVICE, //"http://localhost:5006",
+    payment: process.env.PAYMENT_SERVICE, //"http://localhost:5007",
     pickup: "http://localhost:5008",
     warehouse: "http://localhost:5009",
     hub: "http://localhost:5010",
